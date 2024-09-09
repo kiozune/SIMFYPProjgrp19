9e82e332a770464264c2eea70e2f7f8c940a6075
thekenwong92 <kenneth_wongcw@yahoo.com.sg>
2024-09-09 14:17:48 +0800

    Enemy Drop script v1.1
    fix errors

--- FYPProjGRP19/Assets/Scripts/EnemyLoot/EnemyLoot.cs
+++ FYPProjGRP19/Assets/Scripts/EnemyLoot/EnemyLoot.cs
@@ -4,37 +4,29 @@ using UnityEngine;
 
 public class EnemyLoot : MonoBehaviour
 {
-
     public GameObject EnemyDropModel;
-    public int health = 100;
-    public Transform tansform;
+    public float health = 100f;
 
-
-    if(health<=0)
+    void Update()
     {
-        Destroy(GameObject); // destroy enemy game object
-        DropEnemyLoot(); //drop enemy loot
+        // Check if health is below or equal to 0
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
 
+    void Die()
+    {
+        Destroy(gameObject); // destroy enemy game object
+        DropEnemyLoot(); // drop enemy loot
     }
 
     void DropEnemyLoot() // Enemy loot will drop when enemy is killed
     {
-        Vector3 position = tansform.position; //store current position of enemy
-        GameObject loot = Instantiate(EnemyDropModel, position+ new Vector3(0.0f,1.0f,0.0f),Quaternion.identity); // spawn the loot drop when enmy dies
-        loot.setAcctive(true); //Set the loot object to active
-        Destroy(loot,6f) //Destroy loot drop after 6 seconds
-
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 position = transform.position; // store current position of enemy
+        GameObject loot = Instantiate(EnemyDropModel, position + new Vector3(0.0f, 1.0f, 0.0f), Quaternion.identity); // spawn the loot drop
+        loot.SetActive(true); // Set the loot object to active
+        Destroy(loot, 6f); // Destroy loot drop after 6 seconds
     }
 }
